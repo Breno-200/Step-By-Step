3db96969357c8fcc74a514b6e8208a2008e00844
breno201 <breno.queiroz@websupply.com.br>
2026-01-23 11:44:11 -0300

    feat: provider adicionado

--- step_by_step/lib/main.dart
+++ step_by_step/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 import 'package:step_by_step/app/core/routes/routes.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
